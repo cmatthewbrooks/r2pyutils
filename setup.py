d6534a3ddc5625cee7f594716a5f609d1ef5c99e
cmatthewbrooks <me@cmatthewbrooks.com>
2019-01-08 05:45:45 +0000

    Added initial setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+
+    name = 'r2pyutils',
+    version = '0.1',
+    author = 'cmatthewbrooks',
+    author_email = 'me@cmatthewbrooks.com',
+    keywords = ['radare2'],
+    packages = ['r2pyutils'],
+    entry_points = {
+        'console_scripts': [
+        'funcstrings = r2pyutils.funcstrings:main'
+        ]
+    }
+
+)
