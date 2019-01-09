d56469193d032570fd3b9a74f7a3870265b1ef4c
cmatthewbrooks <me@cmatthewbrooks.com>
2019-01-09 08:22:03 +0000

    Update tests to use absolute path to test exe file

--- tests/test_funcstrings.py
+++ tests/test_funcstrings.py
@@ -1,16 +1,20 @@
+import os
 import unittest
 
 import r2pipe
 
 from context import get_func_strings
 
+TEST_FILE = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'test.exe'
+)
 TEST_STRING_SET = {}
 
 class TestFuncStrings(unittest.TestCase):
 
     def test_get_func_strings(self):
 
-        r2 = r2pipe.open('test.exe')
+        r2 = r2pipe.open(TEST_FILE)
         r2.cmd('aaa')
         
         string_sets = get_func_strings(r2)
--- tests/test_r2ppipeutil.py
+++ tests/test_r2ppipeutil.py
@@ -1,11 +1,14 @@
-import os,sys
+import os
+import sys
 import unittest
 
 import r2pipe
 
 from context import r2pu
 
-
+TEST_FILE = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'test.exe'
+)
 
 class TestR2PPipeUtil(unittest.TestCase):
 
@@ -15,13 +18,13 @@ class TestR2PPipeUtil(unittest.TestCase):
         #
         # AttributeError: 'open' object has no attribute '_cmd'
 
-        self.assertRaises(AttributeError,
+        self.assertRaises(Exception,
                           lambda: r2pu.get_analyzed_r2pipe_from_input())
 
     
     def test_get_analyzed_r2pipe_from_input_with_pipe(self):
         
-        r2 = r2pipe.open("test.exe")
+        r2 = r2pipe.open(TEST_FILE)
         r2 = r2pu.get_analyzed_r2pipe_from_input(r2)
 
         self.assertTrue(int(r2.cmd('aflc')) > 0)
@@ -30,14 +33,14 @@ class TestR2PPipeUtil(unittest.TestCase):
     
     def test_get_analyzed_r2pipe_from_input_with_file(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
 
         self.assertTrue(int(r2.cmd('aflc')) > 0)
         r2.quit()
     
     def test_get_funcj_list(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
         
         funcj_list = r2pu.get_funcj_list(r2)
         
@@ -46,7 +49,7 @@ class TestR2PPipeUtil(unittest.TestCase):
 
     def test_get_function_start_from_offset_with_none(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
         
         # Address 0x40100a belongs to fcn.401000
         r2.cmd("s 0x40100a")
@@ -56,7 +59,7 @@ class TestR2PPipeUtil(unittest.TestCase):
 
     def test_get_function_start_from_offset_with_offset(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
         
         self.assertTrue(hex(r2pu.get_function_start_from_offset(r2,0x40100a)) == '0x401000')
         r2.quit()
@@ -64,7 +67,7 @@ class TestR2PPipeUtil(unittest.TestCase):
     
     def test_get_args_count_to_function_offset_with_none(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
         
         # Address 0x401020 is the 'add' function that takes 2 args
         r2.cmd("s 0x401020")
@@ -74,14 +77,14 @@ class TestR2PPipeUtil(unittest.TestCase):
 
     def test_get_args_count_to_function_offset_with_offset(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
         
         self.assertTrue(int(r2pu.get_args_count_to_function_offset(r2,0x40102a)) == 2)
         r2.quit()
            
     def test_get_call_xref_list_to_function_offset_with_none(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
 
         r2.cmd("s 0x401020")
 
@@ -90,7 +93,7 @@ class TestR2PPipeUtil(unittest.TestCase):
 
     def test_get_call_xref_list_to_function_offset_with_offset(self):
         
-        r2 = r2pu.get_analyzed_r2pipe_from_input("test.exe")
+        r2 = r2pu.get_analyzed_r2pipe_from_input(TEST_FILE)
 
         self.assertTrue(len(r2pu.get_call_xref_list_to_function_offset(r2,0x401020)) == 3)
         r2.quit()
