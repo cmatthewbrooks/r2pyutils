d2b203f2c4a24c457b135bf6b173d95dd2891730
cmatthewbrooks <me@cmatthewbrooks.com>
2019-04-14 02:07:50 -0400

    New version - classes removed in favor of module-level functions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 
     name = 'r2pyutils',
-    version = '0.1',
+    version = '0.2',
     author = 'cmatthewbrooks',
     author_email = 'me@cmatthewbrooks.com',
     keywords = ['radare2'],
